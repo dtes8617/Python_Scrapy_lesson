9278141db9b4cf68b7fd17a2767e1635833cba78
dtes8617 <fortunatemaker2603@gmail.com>
2019-05-02 21:36:43 +0800

    Upload Lesson Resource

---
+++ Lesson/quotes_spider/quotes_spider/spiders/quotes.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = (
+        'http://www.quotes.toscrape.com/',
+    )
+
+    def parse(self, response):
+        pass
