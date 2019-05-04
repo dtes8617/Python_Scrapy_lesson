b11914206b63499f3a0e9cc5c87ad9f3a002ec95
dtes8617 <fortunatemaker2603@gmail.com>
2019-05-04 12:58:47 +0800

    more advanced spider

--- Lesson/quotes_spider/quotes_spider/spiders/quotes.py
+++ Lesson/quotes_spider/quotes_spider/spiders/quotes.py
@@ -6,8 +6,20 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = (
-        'http://www.quotes.toscrape.com/',
+        'http://quotes.toscrape.com/',
     )
 
     def parse(self, response):
-        pass
+        quotes = response.xpath('//*[@class="quote"]')
+        for quote in quotes:
+            text = quote.xpath('.//*[@class="text"]/text()').extract_first()
+            author = quote.xpath('.//*[@itemprop="author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop="keywords"]/@content').extract_first()
+
+            yield{'Text': text,
+                  'Author': author,
+                  'Tags': tags}
+
+        next_page_url = response.xpath('//*[@class="next"]/a/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url)
