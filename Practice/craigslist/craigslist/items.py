a344754696e43d828be3d74cbd56cd66428dfa89
dtes8617 <fortunatemaker2603@gmail.com>
2019-05-15 23:58:17 +0800

    adding Item

--- Practice/craigslist/craigslist/items.py
+++ Practice/craigslist/craigslist/items.py
@@ -11,4 +11,6 @@ import scrapy
 class CraigslistItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    date = scrapy.Field()
+    title = scrapy.Field()
+    urls = scrapy.Field()
--- Practice/craigslist/craigslist/spiders/job_list.py
+++ Practice/craigslist/craigslist/spiders/job_list.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
+from scrapy.loader import ItemLoader
+from craigslist.items import CraigslistItem
 
 
 class JobListSpider(scrapy.Spider):
@@ -8,4 +11,21 @@ class JobListSpider(scrapy.Spider):
     start_urls = ['http://newyork.craigslist.org/search/egr/']
 
     def parse(self, response):
+        l = ItemLoader(CraigslistItem(), response = response)
+
+        date = response.xpath('//time[@class="result-date"]/@datetime').extract()
+        title =  response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        urls = response.xpath('//a[@class="result-title hdrlnk"]/@href').extract()
+
+        for d, t, u in zip(date, title, urls): 
+            l.add_value('date', d)
+            l.add_value('title', t)
+            l.add_value('urls', u)
+            break
+
+        yield l.load_item()
+        # for url in urls:
+        #     yield Request(url, callback = self.parse_details)
+
+    def parse_details(self, response):
         pass
